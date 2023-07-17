87ea446e65cda4070c1015426a29b5e9e0c5c71d
Howard Chen <howardbchen@meta.com>
2023-07-17 16:29:02 -0700

    update package files

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import setuptools
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setuptools.setup(
     name="collie-bench",
     version="0.1.0",
     packages=['collie'],
+    description='Official Implementation of "COLLIE: Systematic Construction of Constrained Text Generation Tasks"',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'nltk>=3.8',
         'openai',
@@ -13,7 +20,6 @@ setuptools.setup(
         'tqdm',
         'apache_beam',
         'tenacity',
-        'google-generativeai'
     ],
     python_requires='>=3.7',
     include_package_data=True,
